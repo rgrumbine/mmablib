79a40f82fe1cdd7167ad702ac4e330906014e6f3
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2022-02-04 16:24:20 +0000

    adding inverse locate and llgrid creator

--- py/grid.py
+++ py/grid.py
@@ -1,8 +1,9 @@
 import numpy as np
-import const
 #Robert Grumbine
 #1 June 2018
 
+#from const import *
+import const
 from ijpt import *
 from latpt import *
 
@@ -21,11 +22,43 @@ class psgrid:
 
 class llgrid:
 
+  def __init__(self, dlat, dlon, firstlat, firstlon, nx, ny):
+    #debug print("hello from llgrid.__init__", flush=True)
+    self.dlat         = dlat
+    self.dlon         = dlon
+    self.firstlat     = firstlat
+    self.firstlon     = firstlon
+    self.nx           = nx
+    self.ny           = ny
+    #debug print("degree area = ",const.degree_area, flush=True)
+    self.darea_base   = abs(self.dlat*self.dlon)*const.degree_area
+    self.dlat_rad     = self.dlat * const.rpdg
+    self.firstlat_rad = self.firstlat * const.rpdg
+
   def locate(self, i, j, z):
     z.lat = self.firstlat + j*self.dlat
     z.lon = self.firstlon + i*self.dlon
     while (z.lon > 360.):
       z.lon -= 360.
+
+  def inv_locate(self, lat, lon):
+    j = (lat - self.firstlat)/self.dlat
+    if (lon < 0):
+      i = self.nx - (self.firstlon - lon)/self.dlon
+    else:
+      i = (lon - self.firstlon)/self.dlon
+
+    return(i,j)
+
+  def cellarea(self, j, i):
+    #Original:
+    #tlat = self.firstlat + j*self.dlat
+    #dy = (self.dlat) * 111.1
+    #dx = (self.dlon) * 111.1 * cos(tlat*const.rpdg )
+    #return abs(dx*dy)
+    #Promote/pre-compute grid constants (darea_base) and
+    #    pre-translate degrees to radians in the base class (*lat_rad)
+    return (self.darea_base * cos(self.firstlat_rad + j*self.dlat_rad) )
 
 #  def locate(self, i, j):
 #    z = latpt()
