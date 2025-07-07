3551a770a2b6045b0c9b6e5b3b3844574d1c2581
Robert Grumbine <Robert.Grumbine@noaa.gov>
2025-07-07 13:38:29 +0000

    add ims4km class

--- include/ncepgrids.h
+++ include/ncepgrids.h
@@ -732,6 +732,41 @@ ims_north<T>::ims_north(int n) {
 
   return ;
 }
+// IMS 4 km grid is not exactly bedient 7 July 2025
+template<class T>
+class ims4km : public psgrid<T> {
+  public:
+    ims4km();
+};
+template<class T>
+ims4km<T>::ims4km() {
+  this->nx = 6144;
+  this->ny = 6144;
+  this->dx = 4000.;
+  this->dy = 4000.;
+  this->slat = 60.0;
+  this->slon = -10.0;
+  this->xorig = -(this->nx/2)*this->dx ;
+  this->yorig = -(this->ny/2)*this->dy ;
+  this->sgn   = 1.0;
+
+  double eccen2 = parameters::eccen2;
+  double eccen  = sqrt(eccen2);
+  this->sl = this->slat / parameters::degrees_per_radian;
+  this->cm = cos(this->sl)/ sqrt(1.0-eccen2*sin(this->sl)*sin(this->sl) );
+  this->tnaught  = tan(M_PI_4 - this->sl/2.) /
+           pow(  ((1.0 - eccen*sin(this->sl))/(1.0+eccen*sin(this->sl))), eccen/2.);
+
+  ijpt f;
+  f.i = 0; f.j = 0;
+  this->first_longitude = (this->locate(f)).lon;
+
+  this->grid = new T[this->nx*this->ny];
+  if (this->grid == (T *) NULL) { cout << "Failed to new in ims_north(void)\n";
+    cout.flush();}
+
+  return ;
+}
 
 // RIPS (Environment Canada Regional Ice Prediction System
 template<class T>
